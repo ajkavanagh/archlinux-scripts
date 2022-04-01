7573d5f4acd79690b4c1e785cbe38efa665dfcd7
Alex Kavanagh <alex@ajkavanagh.co.uk>
2022-04-01 20:27:48 +0100

    Make installing of packages work by checking if they exist first

--- 04-install-pacstrap-packages.sh
+++ 04-install-pacstrap-packages.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-set -ex
+set -e
 
 source ./vars.sh
 source ./utils.sh
@@ -12,15 +12,68 @@ is_mounted "/mnt" && {
 
 # Now with the normal installation (but with extra btrfs packages)
 # base
+declare -a groups=(
+    base-devel
+)
+
 # TODO: this mentions grub, but we are not using it.
-pacstrap /mnt base linux linux-firmware cryptsetup btrfs-progs grub \
-    grub-btrfs intel-ucode dosfstools efibootmgr e2fsprogs \
-    e2fsprogs man-db man-pages texinfo efibootmgr
-# networking
-pacstrap /mnt wpa_supplicant wireless_tools netctl dialog networkmanager
-# editors
-pacstrap /mnt neovim vim
-# extras
-pacstrap /mnt base-devel gptfdisk util-linux
-# laptop extras
-pacstrap /mnt tlp powertop
+declare -a pkgs=(
+    base
+    linux
+    linux-firmware
+    cryptsetup
+    btrfs-progs
+    grub
+    grub-btrfs
+    intel-ucode
+    dosfstools
+    efibootmgr
+    e2fsprogs
+    man-db
+    man-pages
+    texinfo
+    efibootmgr
+    wpa_supplicant
+    wireless_tools
+    netctl
+    dialog
+    networkmanager
+    neovim
+    vim
+    gptfdisk
+    util-linux
+    tlp
+    powertop
+)
+
+install_pkgs=""
+for pkg in "${pkgs[@]}"
+do
+    echo "Looking at $pkg"
+    # or do whatever with individual element of the array
+    arch-chroot /mnt pacman -Q $pkg || {
+        echo "add $pkg"
+        install_pkgs="$install_pkgs $pkg"
+    }
+done
+
+if [[ -n "$install_pkgs" ]]; then
+    echo "Need to install:"
+    echo $install_pkgs
+    pacstrap /mnt $install_pkgs
+fi
+
+# now looks at the groups
+install_groups=""
+for group in "${groups[@]}"; do
+    echo "Looking at group $group"
+    arch-chroot /mnt pacman -Qg $group || {
+        echo "add group $group"
+        install_groups="$install_groups $group"
+    }
+done
+
+if [[ -n "$install_groups" ]]; then
+    echo "Installing groups:"
+    echo $install_groups
+fi
